380cb58b00ab94cc7ee2302a6438e643ede846c4
Ji Fang <fangji@me.com>
2016-08-18 10:32:15 -0700

    [Android] Initial project setup - minimal SDK level 19 (Lollipop)

---
+++ app/android/app/src/main/java/lonelyarcher/com/goshare/android/MainActivity.java
@@ -0,0 +1,13 @@
+package lonelyarcher.com.goshare.android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
